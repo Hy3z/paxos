793046f077226a9082de7c83fb5453681f097bd4
Hy3z <axel.vivenot@outlook.fr>
2025-03-17 13:14:12 +0100

    basic structure

--- src/main/java/paxos/Paxos.java
+++ src/main/java/paxos/Paxos.java
@@ -1,8 +1,19 @@
 package paxos;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 public class Paxos {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        new Paxos().run();
+    }
+
+    private void run() {
+        final ActorSystem system = ActorSystem.create("system");
+        System.out.println("Running...");
+        system.terminate();
+        System.out.println("Terminated.");
     }
 }
---
+++ src/main/java/paxos/Process.java
@@ -0,0 +1,26 @@
+package Paxos;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class Process extends AbstractActor {
+
+    public static Props props() {
+        return Props.create(Process.class);
+    }
+
+    private final LoggingAdapter log = Logging.getLogger(
+        getContext().getSystem(),
+        this
+    );
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+            //.match(.class, this::)
+            .build();
+    }
+}
