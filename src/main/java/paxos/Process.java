7ff0524723bc24121545342fbc7dac944ccdb08b
bob <bobdurant283@gmail.com>
2025-03-18 09:46:02 +0100

    rajout des classes dans process.java

--- src/main/java/paxos/Process.java
+++ src/main/java/paxos/Process.java
@@ -13,14 +13,41 @@ public class Process extends AbstractActor {
     }
 
     private final LoggingAdapter logger = Logging.getLogger(
-        getContext().getSystem(),
-        this
-    );
+            getContext().getSystem(),
+            this);
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-            //.match(.class, this::)
-            .build();
+                // .match(.class, this::)
+                .build();
     }
+
+    // certain entier peuvent être nul, on mettre une valeur très petit (min_int ?)
+    // pour ces valeurs
+    // ballot peut être négatif
+    public record Message_READ(int ballot) {
+    }
+
+    // ballot peut être négatif
+    public record Message_ABORT(int ballot) {
+    }
+
+    // ballot et imposeballot peut être négatif
+    public record Message_GATHER(int ballot, int imposeballot) {
+    }
+
+    // ballot peut être négatif
+    public record Message_ACK(int ballot) {
+    }
+
+    // ballot peut être négatif, proposal peut être "nil"
+    public record Message_IMPOSE(int ballot, int proposal) {
+
+    }
+
+    // value est une valeur entre 0 et 1
+    public record Message_DECIDE(int value) {
+    }
+
 }
